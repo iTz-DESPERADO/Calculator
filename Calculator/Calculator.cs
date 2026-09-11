d3a0a4ff790394518429ab0105b80ba512c5d5fb
Gamal khald <desperadojemy@gmail.com>
2026-09-11 04:09:38 +0200

    add calculator core

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculator;
+
+public static class Calculator
+{
+    public static double Calculate(double firstNumber, double secondNumber, Operation operation)
+    {
+        return operation switch
+        {
+            Operation.Add => firstNumber + secondNumber,
+            Operation.Subtract => firstNumber - secondNumber,
+            Operation.Multiply => firstNumber * secondNumber,
+            Operation.Divide =>  firstNumber / secondNumber,
+            _ => throw new ArgumentOutOfRangeException(nameof(operation), operation, null)
+        };
+    }
+}
+
+public enum Operation
+{
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+public static class OperationExtensions
+{
+    public static string ToSymbol(this Operation operation)
+    {
+        return operation switch
+        {
+            Operation.Add => "+",
+            Operation.Subtract => "-",
+            Operation.Multiply => "*",
+            Operation.Divide => "/",
+            _ => "?"
+        };
+    }
+}
